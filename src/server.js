15ee83ee4dde153874c631bbd0e2b74dbd075d36
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-22 20:54:55 +0900

    2.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,14 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
+
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
-app.get("/", gossipMiddleware, handleHome);
+
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
