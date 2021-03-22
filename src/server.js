b3a52973499a9d27383b458275396d111793ba7c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-22 20:11:07 +0900

    2.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
-
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
