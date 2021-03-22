7c1f2ac47b0f497b8fd706fa4af3e2623c77edd5
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-22 19:40:01 +0900

    2.0 Your First Server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
