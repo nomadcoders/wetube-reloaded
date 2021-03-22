804fd13054117c8c92a633636d6e12d7e49cc48a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-22 19:58:57 +0900

    2.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
