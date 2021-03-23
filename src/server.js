bf88a89b89426248c045fd999817fa57cc532e2d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-23 16:55:17 +0900

    2.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-app.get("/", logger, handleHome);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
