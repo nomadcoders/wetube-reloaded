415297384251492b8bda1028097e8e3d474b9b8e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-25 21:53:37 +0900

    4.0 Returning HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,16 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) =>
+  res.send(
+    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
+  );
 export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  );
 };
 export const edit = (req, res) => {
-  return res.send("Edit");
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  );
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
