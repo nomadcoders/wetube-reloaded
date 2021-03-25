c93aa16c37f20c32559e38e710fa3920761eb85a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-25 16:25:25 +0900

    3.8 URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
