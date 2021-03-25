73e11c623ea8ce7f86cedf82795e0f2ab06ba480
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-25 15:58:52 +0900

    3.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+  return res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
