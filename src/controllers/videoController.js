26884b3b3bb1df92eb16077b1c48c1868ff1ed72
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-25 23:15:11 +0900

    4.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
-};
-export const edit = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
