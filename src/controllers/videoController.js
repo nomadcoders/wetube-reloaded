57110d0f573b94b8c2c6f9551fc48393bdc550cb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-27 21:09:34 +0900

    4.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
