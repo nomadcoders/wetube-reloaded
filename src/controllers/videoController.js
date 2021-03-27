9f547c3f805668d876c11b33d0d0668eb7b07091
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-27 21:54:00 +0900

    4.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
