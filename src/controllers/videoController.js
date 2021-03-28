f59535487bf868f6a07f669fca3cebf0cc98923b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-28 21:13:37 +0900

    4.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,28 @@
 export const trending = (req, res) => {
   const videos = [
     {
-      title: "Hello",
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
-      title: "Video #2",
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
-      title: "Whatsup",
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
