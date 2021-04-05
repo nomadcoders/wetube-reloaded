d6c2c5342e2265d83f811ecd537f63a1f026ccb1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-05 20:49:05 +0900

    5.12 Our First Query part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,12 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I finish first");
 };
 export const watch = (req, res) => {
   const { id } = req.params;
