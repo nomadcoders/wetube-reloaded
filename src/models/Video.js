dc4690a0caba9b2032d10c2b748200cd30a9cdc4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-05 19:52:02 +0900

    5.10 Video Model

---
+++ src/models/Video.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  createdAt: Date,
+  hashtags: [{ type: String }],
+  meta: {
+    views: Number,
+    rating: Number,
+  },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
