883c0aed49d332791c3748f264558138c764dcbe
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-05 17:42:17 +0900

    5.5 More Practice part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,3 +44,12 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array.
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
+  getUpload,
   getEdit,
   postEdit,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
