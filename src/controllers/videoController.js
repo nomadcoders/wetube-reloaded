fb693870467ca0eb7c1ce3386a89cc774119bb02
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-06 15:38:17 +0900

    5.16 Creating a Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,18 +34,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    title: title,
-    description: description,
-    createdAt: Date.now(),
+  await Video.create({
+    title,
+    description,
+    createdAt: "lsdlddldlldld",
     hashtags: hashtags.split(",").map((word) => `#${word}`),
     meta: {
       views: 0,
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
