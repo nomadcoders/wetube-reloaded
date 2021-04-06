79c925dcf8875f623d2b657ff2d6af331970808f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-07 00:23:24 +0900

    5.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,10 +65,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
-    // search
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
   }
-  return res.render("search", { pageTitle: "Search" });
+  return res.render("search", { pageTitle: "Search", videos });
 };
