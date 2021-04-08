e253ac4f536d3cab0aab920779a6d86320f3c3b6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-08 20:36:03 +0900

    6.3 Form Validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,21 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
   await User.create({
     name,
     username,
