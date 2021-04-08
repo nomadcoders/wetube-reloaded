707bb848dd86e2d340a8f9e86b28ccfc25a1bd39
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-08 16:55:59 +0900

    6.1 Create Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,16 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.end();
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/db.js
+++ src/db.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
