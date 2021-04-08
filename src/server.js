ccba950d7f5999fd376fc80d41554678cb40d444
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-08 22:38:37 +0900

    6.8 Sessions and Cookies part Two

--- src/server.js
+++ src/server.js
@@ -28,6 +28,11 @@ app.use((req, res, next) => {
   });
 });
 
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id} ${req.session.potato}`);
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
