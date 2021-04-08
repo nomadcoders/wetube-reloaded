5e038d35724674b22e7114ef7f1f5188995d03a0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-08 22:52:57 +0900

    6.9 Logged In User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,6 +54,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
