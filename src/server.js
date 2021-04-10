1f479d0d2f9a7290e9b48899413e5632c2f80f2e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-10 16:20:02 +0900

    6.13 Uninitialized Sessions

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
