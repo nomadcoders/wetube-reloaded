dd535ad3969555c8812a57ef58c4d1157ae37f19
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-15 16:06:48 +0900

    7.3 Edit Profile POST part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,13 +147,18 @@ export const postEdit = async (req, res) => {
     },
     body: { name, email, username, location },
   } = req;
-  await User.findByIdAndUpdate(_id, {
-    name,
-    email,
-    username,
-    location,
-  });
-  return res.render("edit-profile");
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
 
 export const see = (req, res) => res.send("See User");
