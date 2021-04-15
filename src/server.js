e0895a43fdf2b4327d55f92c789d89e44e74489d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-15 21:32:52 +0900

    7.8 Static Files and Recap

--- src/server.js
+++ src/server.js
@@ -14,7 +14,6 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
@@ -23,8 +22,8 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
-
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
