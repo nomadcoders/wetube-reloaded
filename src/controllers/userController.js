7a0901b9406777dd1b090a500f1591bce8d5286b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-17 17:37:04 +0900

    7.10 User Profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -195,4 +195,14 @@ export const postChangePassword = async (req, res) => {
   return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
+  }
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+  });
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -32,6 +32,6 @@ userRouter
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 
 export default userRouter;
