768d3c0070e8c79f6e710343ea9a9335e27ab8a9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-17 20:04:09 +0900

    7.15 Conclusions

--- src/models/Video.js
+++ src/models/Video.js
@@ -8,7 +8,6 @@ const videoSchema = new mongoose.Schema({
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
-    rating: { type: Number, default: 0, required: true },
   },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
