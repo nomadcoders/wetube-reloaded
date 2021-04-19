7adea0801c328fc2c608955117afafd6a9dfc91a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-19 19:15:46 +0900

    8.4 SCSS Loader

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,2 @@
-alert("hi");
+import "../scss/styles.scss";
+console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
