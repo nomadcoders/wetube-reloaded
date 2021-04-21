f94c56ff19a19b6dc69d512dc0778fe08c156f2a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-21 23:40:16 +0900

    10.0 Player Setup

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,2 @@
 import "../scss/styles.scss";
-console.log("it works");
+console.log("main");
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: "development",
   watch: true,
   plugins: [
@@ -11,7 +14,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
