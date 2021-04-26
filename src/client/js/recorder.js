588990290793efbd80b823eb6c129d07c52e22fe
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-26 22:54:16 +0900

    12.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
