0f3286b57ed702fe10870919fb3196bf6e1c8c07
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-27 22:49:07 +0900

    13.2 Download Transcoded Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -14,9 +14,15 @@ const handleDownload = async () => {
 
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
 };
