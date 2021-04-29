b0280f62ad0708c00fd65087b93b2d58d19f0ed2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-29 19:55:46 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
