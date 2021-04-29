c6ffbad6dac380622fdd4a81b3e4d962d4b60666
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-04-29 14:47:17 +0900

    15.8 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,7 +22,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,6 +30,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
